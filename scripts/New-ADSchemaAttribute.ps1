d20bc7775ff37b0592d6b80c05e6f549c7b42c14
Andy Schneider <andy@mimdev.com>
2017-04-13 05:34:00 +0200

    add new class

--- scripts/New-ADSchemaAttribute.ps1
+++ scripts/New-ADSchemaAttribute.ps1
@@ -1,3 +1,60 @@
 Function New-ADSchemaAttribute {
+
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+    param(
+
+        [Parameter(Mandatory, ValueFromPipelinebyPropertyName)]
+        $Name,
+
+        [Parameter(Mandatory, ValueFromPipelinebyPropertyName)]
+        [Alias('DisplayName')]
+        $LDAPDisplayName,
+
+        [Parameter(Mandatory, ValueFromPipelinebyPropertyName)]
+        [Alias('Description')]
+        $AdminDescription,
+
+        [Parameter(Mandatory, ValueFromPipelinebyPropertyName)]
+        [Alias('SingleValued')]
+        $IsSingleValued,
+
+        [Parameter(ValueFromPipelinebyPropertyName)]
+        [Alias('OID')]
+        $AttributeID 
+    )
+
+    BEGIN {}
+
+    PROCESS {
+  
+        $schemaPath = (Get-ADRootDSE).schemaNamingContext       
+        $type = 'attributeSchema'
+        switch ($isSingleValued) {
+            'True' {$IsSingleValued = $true}
+            'False' {$IsSingleValued = $false}
+            default {$IsSingleValued = $true}
+        }
+
+        $attributes = @{
+            lDAPDisplayName = $Name;
+            attributeId = $AttributeID;
+            oMSyntax = 20;
+            attributeSyntax = "2.5.5.4";
+            isSingleValued = $IsSingleValued;
+            adminDescription = $AdminDescription;
+            searchflags = 1
+        }
+    
+        $ConfirmationMessage = "$schemaPath. This cannot be undone"
+        $Caption = 'Updating Active Directory Schema'
+    
+        if ($PSCmdlet.ShouldProcess($ConfirmationMessage, $Caption)) {
+            New-ADObject -Name $Name -Type $type -Path $schemapath -OtherAttributes $attributes 
+          #  $userSchema = get-adobject -SearchBase $schemapath -Filter 'name -eq "user"'
+          #  $userSchema | Set-ADObject -Add @{mayContain = $Name}
+        }
+    }
+
+    END {}
     
 }
--- scripts/New-ADSchemaClass.ps1
+++ scripts/New-ADSchemaClass.ps1
@@ -1,3 +1,69 @@
 Function New-ADSchemaClass {
 
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+    param(
+
+        [Parameter(Mandatory, ValueFromPipelinebyPropertyName)]
+        $Name,
+
+        [Parameter(Mandatory, ValueFromPipelinebyPropertyName)]
+        [Alias('DisplayName')]
+        $LDAPDisplayName,
+
+        [Parameter(Mandatory, ValueFromPipelinebyPropertyName)]
+        $AdminDisplayName,
+
+        [Parameter(Mandatory, ValueFromPipelinebyPropertyName)]
+        [Alias('Description')]
+        $AdminDescription,
+
+        [Parameter(Mandatory, ValueFromPipelinebyPropertyName)]
+        [ValidateSet("Auxiliary","Abstract","Structural","88 Class")]
+        $Category,
+
+        [Parameter(ValueFromPipelinebyPropertyName)]
+        [Alias('OID')]
+        $AttributeID 
+    )
+
+    BEGIN {}
+
+    PROCESS {
+  
+        $schemaPath = (Get-ADRootDSE).schemaNamingContext       
+        
+
+        switch ($Category) {
+            'Auxiliary'     {$ObjectCategory = 3}
+            'Abstract'      {$ObjectCategory = 2}
+            'Structural'    {$ObjectCategory = 1}
+            '88 Class'      {$ObjectCategory = 0}
+        }
+
+        $attributes = @{
+            governsId = $AttributeID
+            adminDescription = $AdminDescription
+            objectClass =  'classSchema'
+            ldapDisplayName = $LDAPDisplayName
+            adminDisplayName =  $AdminDisplayName
+            objectClassCategory = $ObjectCategory
+            systemOnly =  $FALSE
+            # subclassOf: top
+            subclassOf = 2.5.6.0
+            # rdnAttId: cn
+            rdnAttId = 2.5.4.3
+        }
+    
+        $ConfirmationMessage = "$Name in $schemaPath. This cannot be undone"
+        $Caption = 'Adding a new class to Active Directory Schema'
+    
+        if ($PSCmdlet.ShouldProcess($ConfirmationMessage, $Caption)) {
+            New-ADObject -Name $Name -Type 'classSchema' -Path $schemapath -OtherAttributes $attributes 
+          #  $userSchema = get-adobject -SearchBase $schemapath -Filter 'name -eq "user"'
+          #  $userSchema | Set-ADObject -Add @{mayContain = $Name}
+        }
+    }
+
+    END {}
+    
 }
