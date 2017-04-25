e7c83a87ed872339ba813a96c540705d947a00b4
jonathanweinberg <jonathanweinberg@noreply.github.com>
2017-04-25 01:19:49 -0400

    Instruction cleanup Attrib to Class

--- scripts/Add-ADSchemaAttributeToClass.ps1
+++ scripts/Add-ADSchemaAttributeToClass.ps1
@@ -3,28 +3,31 @@
     Adds an attribute to a class
 
 .DESCRIPTION
-    Add a new custom class to an existing structural class in AD. For example,
-    if you want to add attributes to the user class, you should craete a new
-    auxiliary class, add attributes to that, and then assign the new class as 
-    an aux class to the user class.
+    Add a new custom class to an existing structural class in Active Directory.
+    
+    For example if you want to add attributes to the User Class:
+    1. Create a new Auxiliary Class
+    2. Add Attributes to that new Auxiliary Class.
+    3. Assign the new class as an Auxiliary Class to the User Class.
 
 .PARAMETER AuxiliaryClass
-    The class that will be holding the new attributes you are creating.This 
-    will be an auxiliary class of the structural class.
+    The class that will be holding the new attributes you are creating.
+    This will be an Auxiliary Class of the structural class.
 
 .PARAMETER Class
-    The structural class you are adding an aux class to. 
+    The structural class you are adding an Auxiliary Class to. 
 
 .EXAMPLE
     PS> Add-ADSchemaAuxiliaryClassToClass -AuxiliaryClass asTest -Class User
-    Set the asTest class as an aux class of the User class.
+    Set the 'asTest' class as an Auxiliary Class of the User Class.
 #>
+
 Function Add-ADSchemaAttributeToClass {
 param(
     $Attribute,
     $Class
 )
     $schemaPath = (Get-ADRootDSE).schemaNamingContext  
-    $Schema = get-adobject -SearchBase $schemapath -Filter "name -eq `'$Class`'"
+    $Schema = Get-ADObject -SearchBase $schemaPath -Filter "name -eq `'$Class`'"
     $Schema | Set-ADObject -Add @{mayContain = $Attribute}
 }
